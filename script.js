f92b4b45135b17087cba73962a0ec1dda9b2d96f
Shindanai Sudprasert <smartsoodprasert69@gmail.com>
2024-12-03 15:21:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+console.log("Hello World")
+console.log("Hello World")
+console.log("Hello World")
+
+ alert("Hello")
+
+var
+let
+
+var nameI = "Smart"
+var age = 17
+
+console.log(nameI)
+name = "Smart"
+alert(name)
+
+var surname = "Shindanai"
+var lastname = "Sudprasert"
+console.log(surname, lastname)
+console.log(typeof age)
+alert(typeof true)
+
+var x = 5
+var y = 5 
+var z = x+y 
+console.log(z)
+
+z = x-y
+console.log(z)
+
+z = x*y 
+console.log(z)
+
+z = x/y
+console.log(z)
+
+z = surname + lastname
+console.log(z)
+
+z = surname + x
+console.log(z)
+console(typeof z)
+
+
+{
+    var r = 10
+}
+z = r * r 
+console.log(z)
+
+const n = 99
+console.log(n)
+n = 1
+console.log(n)
+
+var name = prompt("Enter Your Username")
+console.log(name)
+
+var age = prompt("What Is Your Age?")
+console.log(age)
+
+var gender = prompt("What Is Your Gender: Male/Female")
+console.log(gender)
+
+var ran = Math.floor(Math.random() * 100);
+console.log(ran);
+
+document.getElementById("result").innerHTML = ran;
+
+var ans = prompt("Guess the number from 1-100")
+
+var ran = Math.floor(Math.random() * 100);
+console.log(ran)
+if(ans == ran){
+    document.getElementById("result2").innerHTML = "Correct!"
+}
+else {
+    document.getElementById("result2").innerHTML = "Incorrect!"
+}
+
+document.getElementById("result").innerHTML = ran;
+
+document.getElementById("vdo").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/i5-AbnklaGw?si=kWwKlQTp9VWHH9ZG\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
